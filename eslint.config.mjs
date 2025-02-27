5bc533991795581299ae55956f640a860620493e
João Paulo de Fáveri <joao.faveri@gmail.com>
2025-02-27 17:10:40 -0300

    feat(lint): inclusão de arquivos json no eslint
    Ainda falta ajustar as configurações para permitir que ocorra a formatação ao salvar

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,61 +1,69 @@
-import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import json from '@eslint/json'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import tseslint from 'typescript-eslint'
 
 export default [
   js.configs.recommended,
+  json.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
   {
-    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         project: './tsconfig.json',
         ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
-      'prettier': prettierPlugin,
+      'json': json,
+      'prettier': prettierPlugin
     },
     rules: {
       // TypeScript specific rules
       '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_' 
-      }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
       '@typescript-eslint/no-explicit-any': 'warn',
 
       // Prettier
-      'prettier/prettier': ['warn', {
-        arrowParens: "avoid",
-        bracketSpacing: true,
-        htmlWhitespaceSensitivity: "css",
-        insertPragma: false,
-        bracketSameLine: true,
-        jsxSingleQuote: true,
-        printWidth: 80,
-        proseWrap: "always",
-        quoteProps: "as-needed",
-        requirePragma: false,
-        semi: false,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: "none",
-        useTabs: false,
-        experimentalTernaries: true,
-        experimentalOperatorPosition: "start",
-        objectWrap: "preserve",
-        endOfLine: "lf",
-        singleAttributePerLine: true
-      }],
-    },
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'avoid',
+          bracketSpacing: true,
+          htmlWhitespaceSensitivity: 'css',
+          insertPragma: false,
+          bracketSameLine: true,
+          jsxSingleQuote: true,
+          printWidth: 80,
+          proseWrap: 'always',
+          quoteProps: 'consistent',
+          requirePragma: false,
+          semi: false,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'none',
+          useTabs: false,
+          experimentalTernaries: true,
+          experimentalOperatorPosition: 'start',
+          objectWrap: 'preserve',
+          endOfLine: 'lf',
+          singleAttributePerLine: true
+        }
+      ]
+    }
   },
   {
-    ignores: ['dist/', 'build/', 'node_modules/'],
-  },
-];
+    ignores: ['dist/', 'build/', 'node_modules/', './CHANGELOG.md']
+  }
+]
